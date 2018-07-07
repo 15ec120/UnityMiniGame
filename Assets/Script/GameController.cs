e2c01ddc5e3bd81b6f19fdbfcf4f87ce2f7749d5
Takuyahara <15ec097@ms.dendai.ac.jp>
2018-07-07 21:36:49 +0900

    1

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    int score = 0;
+    public UnityEngine.UI.Text scoreLabel;
+
+    private void OnTriggerEnter(Collider hit){
+        //Cubeに触れると100点追加
+        if (hit.CompareTag("Cube"))
+        {
+            score += 100;
+        }
+
+        //Capsuleに触れると50点追加
+        else if (hit.CompareTag("Capsule"))
+        {
+            score += 50;
+        }
+    }
+
+    public void Update(){
+        scoreLabel.text = "Score:" + score.ToString();
+    }
+}
---
+++ Assets/Script/ItemScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
